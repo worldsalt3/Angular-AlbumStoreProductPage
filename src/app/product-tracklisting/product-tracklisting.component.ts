f0e6f8d5b0d4f98878dcd9dede1294063eed149e
Worldsalt3 <riliwanhazzan@gmail.com>
2019-11-08 05:27:36 +0100

    Complete module

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
 import { Album } from '../album';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
+export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
   constructor(private _productService: ProductService) { }
@@ -15,5 +15,6 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
     this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
+
 
 }
